b453e6828e4c43c33e2aba224d4bfd5b61853798
Euphemia Deathridge <ephdtrg@gmail.com>
2020-01-28 16:35:42 +0300

    save transfers to db

--- ducatus_widget/transfers/api.py
+++ ducatus_widget/transfers/api.py
@@ -1,4 +1,6 @@
 from ducatus_widget.litecoin_rpc import DucatuscoreInterface
+from ducatus_widget.transfers.models import DucatusTransfer
+from ducatus_widget.exchange_requests.models import ExchangeRequest
 
 
 def transfer_ducatus(payment):
@@ -8,5 +10,16 @@ def transfer_ducatus(payment):
 
     rpc = DucatuscoreInterface()
     res = rpc.transfer(receiver, amount)
+
+    exchange_request = ExchangeRequest.objects.get(id=payment.request.id)
+    transfer = DucatusTransfer(
+        request=exchange_request,
+        # tx_hash=res['tx_hash']
+        amount=amount,
+        payment=payment,
+        state='WAITING_FOR_CONFIRMATION'
+    )
+    transfer.save()
+
     print('ducatus transfer ok')
     return res
