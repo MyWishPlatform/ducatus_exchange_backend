38267d187c653b3dc9d12694a24a00c936298d31
Euphemia Deathridge <ephdtrg@gmail.com>
2020-01-29 14:28:51 +0300

    move key to local settings

--- ducatus_widget/settings.py
+++ ducatus_widget/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%_mw&rzg2jxv^49(iijm$=hdroxwjf_=ckei=ox9d-xh#*=y!b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
