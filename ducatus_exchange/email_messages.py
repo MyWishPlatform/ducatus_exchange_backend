35b9493fbdf9e7510c70d34ec0bc1e8fd7ef96bf
Egor14 <egorgrekhnev@mail.ru>
2020-07-22 13:07:54 +0300

    lottery confitmation mail

---
+++ ducatus_exchange/email_messages.py
@@ -0,0 +1,9 @@
+lottery_subject = """Hello"""
+
+lottery_text = """Hello, you are new lottery player
+Your tx hash {tx_hash}
+Your tickets amount {tickets_amount}"""
+
+promo_codes_text = """
+Your backoffice code {back_office_code}
+Your e-commerce code {e_commerce_code}"""
--- ducatus_exchange/lottery/api.py
+++ ducatus_exchange/lottery/api.py
@@ -1,9 +1,14 @@
+import sys
+import traceback
 from django.utils import timezone
+from django.core.mail import send_mail
 
 from ducatus_exchange.rates.serializers import get_usd_prices
 from ducatus_exchange.lottery.models import Lottery, LotteryPlayer
 from ducatus_exchange.transfers.models import DucatusTransfer
 from ducatus_exchange.consts import TICKETS_FOR_USD, DECIMALS, RATES_PRECISION
+from ducatus_exchange.email_messages import lottery_subject, lottery_text, promo_codes_text
+from ducatus_exchange.settings import CONFIRMATION_FROM_EMAIL
 
 
 class LotteryRegister:
@@ -15,7 +20,8 @@ class LotteryRegister:
     def try_register_to_lotteries(self):
         active_lotteries = self.get_active_lotteries()
         for lottery in active_lotteries:
-            self.register_to_lottery(lottery)
+            lottery_player = self.register_to_lottery(lottery)
+            self.send_confirmation(lottery_player)
 
     def get_active_lotteries(self):
         active_lotteries = Lottery.objects.filter(ended=False, started_at__lt=timezone.now().timestamp())
@@ -49,6 +55,8 @@ class LotteryRegister:
                                                                                             usd_amount, tickets_amount),
             flush=True)
 
+        return lottery_player
+
     def get_tickets_amount(self, usd_amount):
         tickets_amount_result = 0
         for usd_value, tickets_amount in TICKETS_FOR_USD.items():
@@ -65,3 +73,17 @@ class LotteryRegister:
         amount = self.payment.original_amount
         usd_amount = usd_prices[currency] * amount / DECIMALS[currency]
         return usd_amount
+
+    def send_confirmation(self, lottery_player):
+        try:
+            send_mail(
+                lottery_subject,
+                lottery_text.format(
+                    tx_hash=lottery_player.transfer.tx_hash,
+                    tickets_amount=lottery_player.tickets_amount,
+                ),
+                CONFIRMATION_FROM_EMAIL,
+                [lottery_player.transfer.payment.exchange_request.user.email],
+            )
+        except Exception as e:
+            print('\n'.join(traceback.format_exception(*sys.exc_info())), flush=True)
