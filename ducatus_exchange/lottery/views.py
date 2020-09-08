f5d854c0954f35dcc53a211fee0291037598ba1d
Egor14 <egorgrekhnev@mail.ru>
2020-09-08 13:03:23 +0300

    manual payments registration handler

--- ducatus_exchange/lottery/views.py
+++ ducatus_exchange/lottery/views.py
@@ -5,10 +5,13 @@ from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticatedOrReadOnly
-from rest_framework.exceptions import PermissionDenied
+from rest_framework.exceptions import PermissionDenied, APIException
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 from ducatus_exchange.lottery.models import Lottery, LotteryPlayer
 from ducatus_exchange.lottery.serializers import LotterySerializer, LotteryPlayerSerializer
+from ducatus_exchange.lottery.manual_registration import register_payments_data
 from ducatus_exchange.settings import API_KEY
 
 
@@ -45,3 +48,46 @@ def lottery_participants(request: Request):
             response_data.append(serializer.to_representation(player, is_admin=True))
 
     return Response(response_data)
+
+
+@swagger_auto_schema(
+    method='post',
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'api_key': openapi.Schema(type=openapi.TYPE_STRING),
+            'data': openapi.Schema(
+                type=openapi.TYPE_ARRAY,
+                items=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        'username': openapi.Schema(type=openapi.TYPE_STRING),
+                        'quantity': openapi.Schema(type=openapi.TYPE_INTEGER),
+                        'package': openapi.Schema(
+                            type=openapi.TYPE_INTEGER,
+                            enum=[10, 50, 100, 500, 1000]
+                        ),
+                        'address': openapi.Schema(type=openapi.TYPE_STRING),
+                        'email': openapi.Schema(type=openapi.TYPE_STRING),
+                    },
+                    required=['username', 'quantity', 'package', 'address', 'email']
+                )
+            )
+        },
+        required=['api_key', 'data']
+    ),
+)
+@api_view(http_method_names=['POST'])
+def register_payments_manually(request: Request):
+    request_api_key = request.data.get('api_key')
+    if request_api_key != API_KEY:
+        raise PermissionDenied
+
+    payments_data = request.data.get('data')
+
+    try:
+        register_payments_data(payments_data)
+    except:
+        raise APIException
+
+    return Response({'success': 'ok'})
