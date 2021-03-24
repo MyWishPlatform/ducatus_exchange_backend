db7b88d5a78f249a22cdfef45464b35e54241558
dsahdr <3180Gmtu>
2021-03-24 17:23:13 +0300

    implemented api for HDWallet last id additionally fixed rate limits for develop in consts.py

--- ducatus_exchange/exchange_requests/urls.py
+++ ducatus_exchange/exchange_requests/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-
-from ducatus_exchange.exchange_requests.views import ExchangeRequestView
+from django.urls import path
+from ducatus_exchange.exchange_requests.views import ExchangeRequestView, total_id_count
 
 urlpatterns = [
+    path('total/', total_id_count),
     url(r'^$', ExchangeRequestView.as_view(), name='create-exchange-request'),
 ]
