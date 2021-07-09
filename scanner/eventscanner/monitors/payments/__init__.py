f5454f0ee35a67a8e97a44b16c530c82dae18bcd
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2021-07-09 11:45:10 +0300

    add scanner to project

---
+++ scanner/eventscanner/monitors/payments/__init__.py
@@ -0,0 +1,4 @@
+from .btc import BTCPaymentMonitor, DucPaymentMonitor
+from .erc20 import ERC20PaymentMonitor
+from .eth_payment_monitor import EthPaymentMonitor, DucxPaymentMonitor
+from .duc_voucher_payment_monitor import DucVoucherPaymentMonitor
