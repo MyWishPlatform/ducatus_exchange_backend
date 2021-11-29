38ee56977c1f34b6d0fb246af3a3959ff442abbb
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-11-29 15:19:12 +0300

    rename field

--- ducatus_exchange/bot/models.py
+++ ducatus_exchange/bot/models.py
@@ -7,7 +7,7 @@ class BotSub(models.Model):
 
 
 class BotSwapMessage(models.Model):
-    swap = models.ForeignKey('payments.Payment', on_delete=models.CASCADE)
+    payment = models.ForeignKey('payments.Payment', on_delete=models.CASCADE)
     sub = models.ForeignKey(BotSub, on_delete=models.CASCADE)
     message_id = models.IntegerField(default=0)
 
