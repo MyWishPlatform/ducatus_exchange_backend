9a7d3b0b6c491d7b2d04897536d740a9b32addfd
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-12-01 13:11:50 +0300

    update django static url

--- ducatus_exchange/settings.py
+++ ducatus_exchange/settings.py
@@ -144,7 +144,7 @@ STATS_NORMALIZED_TIME = '%Y-%m-%dT%H:%M:%S.%fZ'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'ducatus_exchange', 'static/')
-STATIC_URL = '/static/'
+STATIC_URL = '/django-static/'
 # STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, 'ducatus_exchange', 'static'),
 # ]
