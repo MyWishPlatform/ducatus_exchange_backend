df4215cf37c19bab457eec12755ee4fc840cfa95
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-12-02 19:37:59 +0300

    refactor

--- ducatus_exchange/exchange_requests/tasks.py
+++ ducatus_exchange/exchange_requests/tasks.py
@@ -10,9 +10,9 @@ from celery_config import app
 @app.task
 def update_duc_and_ducx_balances():
     logger = getLogger('task')
-    exchange_status = ExchangeStatus.objects.first()
-    exchange_status.duc_balance = DucatuscoreInterface().get_balance()
-    exchange_status.ducx_balance = ParityInterface().get_balance()
-    exchange_status.save()
-    logger.info(msg=f'Update DUC and DUCX balamces, everything is OK.')
+    status = ExchangeStatus.objects.first()
+    status.duc_balance = DucatuscoreInterface().get_balance()
+    status.ducx_balance = ParityInterface().get_balance()
+    status.save()
+    logger.info(msg=f'Update DUC and DUCX balances, everything is OK.')
     
