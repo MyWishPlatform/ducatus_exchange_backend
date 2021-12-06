1cb9c23a184fa1fd4ecda00f3380daf419a99a12
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-12-06 17:48:15 +0300

    refactor

--- ducatus_exchange/bot/services.py
+++ ducatus_exchange/bot/services.py
@@ -1,7 +1,6 @@
 import logging
 
 from django.db import transaction
-from urllib.parse import urljoin
 from ducatus_exchange.bot.models import BotSub, BotSwapMessage
 from ducatus_exchange.bot.base import Bot
 from ducatus_exchange.settings import NETWORK_SETTINGS
@@ -17,15 +16,24 @@ def send_or_update_message(payment):
     for sub in subs:
         try:
             message_model, created = BotSwapMessage.objects.select_for_update().get_or_create(
-                payment_id=payment.id, sub=sub
+                payment_id=payment.id, sub=sub,
             )
             if created:
-                message_model.message_id = Bot().bot.send_message(sub.chat_id, message, parse_mode='html',
-                                                                    disable_web_page_preview=True).message_id
+                message_model.message_id = Bot().bot.send_message(
+                    sub.chat_id, 
+                    message, 
+                    parse_mode='html', 
+                    disable_web_page_preview=True
+                ).message_id
                 message_model.save()
             else:
-                Bot().bot.edit_message_text(message, sub.chat_id, message_model.message_id, parse_mode='html',
-                                                disable_web_page_preview=True)
+                Bot().bot.edit_message_text(
+                    message, 
+                    sub.chat_id, 
+                    message_model.message_id, 
+                    parse_mode='html',
+                    disable_web_page_preview=True
+                )
         except Exception as e:
             logger.error(msg=f'send_or_update_message FAILED on payment: {payment.id} with exception: \n {e}')
     logger.info(msg=f'send_or_update_message SUCCEDED on payment: {payment.id}')
